63097282b5305c3c7d8bf8c2817390ab93bdebcb
Aaron Thurston <aaron.thurston27@gmail.com>
2024-08-01 13:04:46 -0400

    Starting work for wind burst ability

---
+++ Source/ThirdPersonProj/Public/TPPFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TPPFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONPROJ_API UTPPFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};

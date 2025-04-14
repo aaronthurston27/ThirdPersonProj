58f8e68b2efdab300abd5036918d1f43a0ccb948
aaronthurston27 <41075873+aaronthurston27@users.noreply.github.com>
2025-04-13 19:26:34 -0700

    Adding melee ability for primary weapons

---
+++ Source/ThirdPersonProj/Public/TPPAbility_MeleeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/TPPAbility.h"
+#include "TPPAbility_MeleeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONPROJ_API UTPPAbility_MeleeBase : public UTPPAbility
+{
+	GENERATED_BODY()
+	
+};
